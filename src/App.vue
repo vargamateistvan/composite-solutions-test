c4c3d663caf5fec5d46810926dbe5db793d4c875
Varga Máté István <mateistvanvarga@gmail.com>
2024-01-19 18:28:43 +0100

    feat: Navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
+  <navigation-bar></navigation-bar>
   <router-view />
 </template>
 
 <script>
-export default {};
+import navigationBar from "./components/navigation-bar.vue";
+export default {
+  components: { navigationBar },
+};
 </script>
---
+++ src/components/navigation-bar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="navigation-bar">
+    <router-link class="navigation-bar-element" to="/todo">Todo</router-link>
+    <router-link class="navigation-bar-element" to="/gallery"
+      >Gallery</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.navigation-bar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: flex-start;
+  background: #213547;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+
+  a.router-link-active {
+    color: white;
+  }
+
+  a:hover {
+    background: white;
+    color: #213547;
+  }
+
+  &-element {
+    color: grey;
+    text-decoration: none;
+    margin-left: 25px;
+    padding: 10px;
+    text-align: center;
+  }
+}
+</style>
